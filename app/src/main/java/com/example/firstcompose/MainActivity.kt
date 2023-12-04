c193c9560619d4c5d95f1034777d21392cae5284
Pranita <pranitapawar0891@gmail.com>
2023-12-04 18:39:08 +0530

    Adding Text and Image property sample

--- app/src/main/java/com/example/firstcompose/MainActivity.kt
+++ app/src/main/java/com/example/firstcompose/MainActivity.kt
@@ -3,13 +3,22 @@ package com.example.firstcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.firstcompose.ui.theme.FirstComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -25,10 +34,21 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    /*Text(text = "Hello $name!",
+        color = Color.Gray,
+        fontStyle = FontStyle.Italic,
+        fontWeight = FontWeight.ExtraBold,
+        textAlign = TextAlign.Center,
+        fontSize = 36.sp
+   )*/
+    Image(painter = painterResource(id = R.drawable.baseline_coffee_24),
+        contentDescription = "$name",
+        colorFilter = ColorFilter.tint(Color.Black),
+        contentScale = ContentScale.Crop
+    )
 }
 
-@Preview(showBackground = true, name = "First Message", widthDp = 200, heightDp = 200, showSystemUi = true)
+@Preview(showBackground = true, name = "First Message",showSystemUi = true)
 @Composable
 fun DefaultPreview() {
     FirstComposeTheme {
